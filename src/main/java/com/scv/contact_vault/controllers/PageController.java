74f166f7542f6fcff27fdd9f43846392efacc33c
Avish <avitmr2345@gmail.com>
2025-01-07 19:17:58 +0530

    form validation added

--- src/main/java/com/scv/contact_vault/controllers/PageController.java
+++ src/main/java/com/scv/contact_vault/controllers/PageController.java
@@ -2,6 +2,7 @@ package com.scv.contact_vault.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import com.scv.contact_vault.helpers.Message;
 import com.scv.contact_vault.helpers.MessageType;
 import com.scv.contact_vault.services.UserService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class PageController {
@@ -59,8 +61,13 @@ public class PageController {
     }
 
     @PostMapping("/signup")
-    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session) {
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult theBindingResult,
+            HttpSession session) {
         System.out.println("Processing registration");
+
+        if (theBindingResult.hasErrors()) {
+            return "register";
+        }
 
         User user = new User();
         user.setName(userForm.getName());
--- src/main/java/com/scv/contact_vault/forms/UserForm.java
+++ src/main/java/com/scv/contact_vault/forms/UserForm.java
@@ -1,5 +1,9 @@
 package com.scv.contact_vault.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +19,23 @@ import lombok.ToString;
 @ToString
 public class UserForm {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, message = "Minimum three characters are required")
     private String name;
+
+    @Email(message = "Invalid Email Address")
+    @NotBlank(message = "Email is required")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@gmail\\.com$", message = "Email must be in a valid format (e.g. xyz@gmail.com)")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Minimum six characters are required")
     private String password;
+
+    @NotBlank(message = "About is required")
     private String about;
+
+    @Size(min = 10, max = 10, message = "Invalid Phone Number")
     private String phoneNumber;
 
 }
