1cef5841103a6f831c18f36bed5e34917bd32d99
Avish <avitmr2345@gmail.com>
2025-01-09 18:46:37 +0530

    removing the authentication provider and using the auto configured Spring Security authentication provider. Also removed a warning

--- src/main/java/com/scv/contact_vault/config/SecurityConfig.java
+++ src/main/java/com/scv/contact_vault/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.scv.contact_vault.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,24 +11,11 @@ import com.scv.contact_vault.services.CustomSecurityUserDetailService;
 @Configuration
 public class SecurityConfig {
 
+    @SuppressWarnings("unused")
     private CustomSecurityUserDetailService userDetailService;
 
     public SecurityConfig(CustomSecurityUserDetailService theCustomSecurityUserDetailService) {
         userDetailService = theCustomSecurityUserDetailService;
-    }
-
-    // configuration of authentication provider for spring security
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-
-        // user detail service object:
-        daoAuthenticationProvider.setUserDetailsService(userDetailService);
-
-        // password encoder object
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-
-        return daoAuthenticationProvider;
     }
 
     @Bean
@@ -39,7 +25,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http.authorizeHttpRequests(authorize -> {
             authorize.requestMatchers("/user/**").authenticated();
             authorize.anyRequest().permitAll();
