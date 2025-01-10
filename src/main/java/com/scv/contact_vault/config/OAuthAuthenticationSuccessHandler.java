c27be09a9d17f393834984fddd2c40e51c3706ba
Avish <avitmr2345@gmail.com>
2025-01-10 16:46:40 +0530

    login with github implemented

--- src/main/java/com/scv/contact_vault/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/scv/contact_vault/config/OAuthAuthenticationSuccessHandler.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -29,47 +30,61 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
 
     Logger logger = LoggerFactory.getLogger(OAuthAuthenticationSuccessHandler.class);
 
+    @SuppressWarnings("null")
     @Override
     public void onAuthenticationSuccess(
             HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
 
-        logger.info("OAuthAuthenicationSuccessHandler");
+        logger.info("OAuthAuthenticationSuccessHandler");
 
-        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+        var oauth2AuthenicationToken = (OAuth2AuthenticationToken) authentication;
+        String authorizedClientRegistrationId = oauth2AuthenicationToken.getAuthorizedClientRegistrationId();
+        logger.info(authorizedClientRegistrationId);
 
-        // logger.info(user.getName());
-        // user.getAttributes().forEach((key, value) -> {
+        var oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
+
+        // logger.info(oauthUser.getName());
+        // oauthUser.getAttributes().forEach((key, value) -> {
         // logger.info("{} => {}", key, value);
         // });
-        // logger.info(user.getAuthorities().toString());
+        // logger.info(oauthUser.getAuthorities().toString());
 
-        String email = user.getAttribute("email").toString();
-        String name = user.getAttribute("name").toString();
-        String picture = user.getAttribute("picture").toString();
+        User user = new User();
+        user.setUserId(UUID.randomUUID().toString());
+        user.setRoleList(List.of(AppConstants.ROLE_USER));
+        user.setEmailVerified(true);
+        user.setEnabled(true);
+        user.setPassword("password");
 
-        if (email != null && name != null && picture != null) {
-            User user1 = new User();
-            user1.setEmail(email);
-            user1.setName(name);
-            user1.setProfilePic(picture);
-            user1.setPassword("password");
-            user1.setUserId(UUID.randomUUID().toString());
-            user1.setProvider(Providers.GOOGLE);
-            user1.setEnabled(true);
-            user1.setEmailVerified(true);
-            user1.setProviderUserId(user.getName());
-            user1.setRoleList(List.of(AppConstants.ROLE_USER));
-            user1.setAbout("This account is created using google.");
+        if (authorizedClientRegistrationId.equalsIgnoreCase("google")) {
 
-            User existingUser = userRepo.findByEmail(email).orElse(null);
-            if (existingUser == null) {
-                userRepo.save(user1);
-                logger.info("User saved:" + email);
-            }
+            user.setEmail(oauthUser.getAttribute("email").toString());
+            user.setProfilePic(oauthUser.getAttribute("picture").toString());
+            user.setName(oauthUser.getAttribute("name").toString());
+            user.setProviderUserId(oauthUser.getName());
+            user.setProvider(Providers.GOOGLE);
+            user.setAbout("This account is created using google.");
+
+        } else if (authorizedClientRegistrationId.equalsIgnoreCase("github")) {
+
+            user.setEmail(oauthUser.getAttribute("email") != null ? oauthUser.getAttribute("email").toString()
+                    : oauthUser.getAttribute("login").toString() + "@gmail.com");
+            user.setProfilePic(oauthUser.getAttribute("avatar_url").toString());
+            user.setName(oauthUser.getAttribute("login").toString());
+            user.setProviderUserId(oauthUser.getName());
+            user.setProvider(Providers.GITHUB);
+            user.setAbout("This account is created using github");
+
         } else {
-            logger.error("OAuth attributes missing. User creation aborted.");
+            logger.info("OAuthAuthenicationSuccessHandler: Unknown provider");
+        }
+
+        User userExist = userRepo.findByEmail(user.getEmail()).orElse(null);
+        if (userExist == null) {
+            userRepo.save(user);
+            System.out.println("User saved: " + user.getEmail());
         }
 
         new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile");
